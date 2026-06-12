e65b21f3d5da6450ab799db943ff6ffdf5ba317c
Akhilesh Pal <159413384+SnipeWipe@users.noreply.github.com>
2026-06-12 22:32:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,30 @@ st.markdown("""
 - Model Monitoring
 - Best Model Hyperparameter Tuning  
 """)
+
+import streamlit as st
+import pandas as pd
+
+st.title("Credit Risk Scorecard Platform")
+
+uploaded_file = st.sidebar.file_uploader(
+    "Upload Dataset",
+    type=["csv"]
+)
+
+if uploaded_file:
+
+    df = pd.read_csv(uploaded_file)
+
+    if "duration" in df.columns:
+        df = df.drop("duration", axis=1)
+
+    if "deposit" in df.columns:
+        df["deposit"] = df["deposit"].map({
+            "no":0,
+            "yes":1
+        })
+
+    st.session_state["df"] = df
+
+    st.success("Dataset Loaded")
