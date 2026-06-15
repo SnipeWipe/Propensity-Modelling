3b940e262820b7b6c08fdcebc8b59ac95c94d8b5
Akhilesh Pal <159413384+SnipeWipe@users.noreply.github.com>
2026-06-15 10:05:12 +0530

    Update Model_Training.py

--- pages/Model_Training.py
+++ pages/Model_Training.py
@@ -12,6 +12,7 @@ from sklearn.metrics import (
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LogisticRegression
@@ -44,13 +45,19 @@ if st.button("Train Models"):
     num_cols = X.select_dtypes(exclude='object').columns.tolist()
 
     preprocessor = ColumnTransformer([
-        ('num', SimpleImputer(strategy='median'), num_cols),
-        ('cat', Pipeline([
-            ('imputer', SimpleImputer(strategy='most_frequent')),
-            ('encoder', OneHotEncoder(handle_unknown='ignore'))
-        ]), cat_cols)
-    ])
-
+    ('num',Pipeline([
+            ('imputer',
+             SimpleImputer(strategy='median')),
+            ('scaler',
+             StandardScaler())
+        ]),num_cols),
+    ('cat',Pipeline([
+            ('imputer',
+             SimpleImputer(strategy='most_frequent')),
+            ('encoder',
+             OneHotEncoder(handle_unknown='ignore'))
+        ]),cat_cols)
+])
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
